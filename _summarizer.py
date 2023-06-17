4895a479271f6a707d54279cd8a9664fee9658c0
TeuvoKinnunen <teuvo.kinnunen@gmx.com>
2023-06-17 14:32:01 +0300

    added encoding

--- _summarizer.py
+++ _summarizer.py
@@ -425,7 +425,7 @@ def save_all(new_book, book_content, dalle_data, stability_data, image_prompt):
         summary_path = f"{folder_path}/{new_book}.txt"
         summary_data = book_content.encode("utf-8")
         prompt_data = image_prompt.encode("utf-8")
-        data_to_txt = f"{summary_data}\nImage prompt:\n{prompt_data}"
+        data_to_txt = f"Summary:\n{summary_data.decode('utf-8')}\n\nImage prompt:\n{prompt_data.decode('utf-8')}"
         dbx.files_upload(data_to_txt.encode("utf-8"), summary_path)
 
         # Save DALL-E images to png
