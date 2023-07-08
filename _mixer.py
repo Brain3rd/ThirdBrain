0afd160078ee60a0f04d0a9b8df3bccc7ea2500e
TeuvoKinnunen <teuvo.kinnunen@gmx.com>
2023-07-08 14:59:17 +0300

    Removed chatbots, added Spoty GPT

---
+++ _mixer.py
@@ -0,0 +1,145 @@
+import environment as env
+import openai
+import json
+import datetime
+import spotipy
+import streamlit as st
+
+
+client_id = env.get_api_key("SPOTIFY_ID")
+client_secret = env.get_api_key("SPOTIFY_SECRET")
+
+# Openai Keys
+openai.api_key = env.get_api_key("OPENAI_API_KEY")
+
+
+def generate_playlist(prompt, count):
+    example_json = """
+      [
+          {"song": "Everybody Hurts", "artist": "R.E.M."},
+          {"song": "Yesterday", "artist": "The Beatles"},
+          {"song": "Tears in Heaven", "artist": "Eric Clapton"},
+          {"song": "Hallelujah", "artist": "Jeff Buckley"},
+          {"song": "Nothing Compares 2 U", "artist": "Sinead O'Connor"}
+      ]
+  """
+
+    messages = [
+        {
+            "role": "system",
+            "content": """
+      You are a helpful playlist generating assistant.
+      You should generate a list of songs and their artists according to a text prompt.
+      You should return a JSON array, where each element follows this format: {"song": <song_title>, "asrtist": <artist_name>}
+      """,
+        },
+        {
+            "role": "user",
+            "content": """
+      Generate a playlist of 5 songs based on this prompt: super super sad songs
+      """,
+        },
+        {
+            "role": "assistant",
+            "content": example_json,
+        },
+        {
+            "role": "user",
+            "content": f"""
+      Generate a playlist of {count}songs based on this prompt: {prompt}
+      """,
+        },
+        {
+            "role": "assistant",
+            "content": "Return just a JSON array, nothing else. No text before, no text after.",
+        },
+    ]
+
+    response = openai.ChatCompletion.create(
+        messages=messages, model="gpt-3.5-turbo", max_tokens=400
+    )
+
+    output = response["choices"][0]["message"]["content"]
+    playlist = json.loads(output)
+    return playlist
+
+
+def spotify_playlist(playlist, playlist_name):
+    sp = spotipy.Spotify(
+        auth_manager=spotipy.SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri="http://localhost:9999",
+            scope="playlist-modify-private",
+        )
+    )
+
+    current_user = sp.current_user()
+
+    assert current_user is not None
+
+    track_uris = []
+    for item in playlist:
+        artist, song = item["artist"], item["song"]
+        # https://developer.spotify.com/documentation/web-api/reference/#/operations/search
+
+        advanced_query = f"artist:({artist}) track:({song})"
+        basic_query = f"{song} {artist}"
+
+        for query in [advanced_query, basic_query]:
+            with st.spinner(f"Searching for query: {query}"):
+                search_results = sp.search(
+                    q=query, limit=10, type="track"
+                )  # , market=market)
+
+            if (
+                not search_results["tracks"]["items"]
+                or search_results["tracks"]["items"][0]["popularity"] < 20
+            ):
+                continue
+            else:
+                good_guess = search_results["tracks"]["items"][0]
+                st.success(f"Found: {good_guess['name']} [{good_guess['id']}]")
+                # print(f"FOUND USING QUERY: {query}")
+                track_uris.append(good_guess["id"])
+                break
+
+        else:
+            st.info(
+                f"Queries {advanced_query} and {basic_query} returned no good results. Skipping."
+            )
+
+    created_playlist = sp.user_playlist_create(
+        current_user["id"],
+        public=False,
+        name=f"{playlist_name} ({datetime.datetime.now().strftime('%c')})",
+    )
+
+    sp.user_playlist_add_tracks(current_user["id"], created_playlist["id"], track_uris)
+
+    st.success(f"Created playlist: {created_playlist['name']}")
+    st.success(created_playlist["external_urls"]["spotify"])
+
+
+def get_user_playlists():
+    sp = spotipy.Spotify(
+        auth_manager=spotipy.SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri="http://localhost:9999",
+            scope="playlist-read-private",
+        )
+    )
+
+    playlists = sp.current_user_playlists(limit=50)
+    all_playlists = []
+
+    while playlists:
+        for playlist in playlists["items"]:
+            all_playlists.append(playlist)
+        if playlists["next"]:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+
+    return all_playlists
---
+++ pages/4_🎵_Spoty_GPT.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+import _mixer as mix
+
+
+TITLE = "Spoti Mixer"
+ABOUT = """
+        🎵 Spoty GPT
+
+        Spoty GPT is an innovative AI-powered tool that seamlessly integrates ChatGPT with Spotify. With Spoty GPT, you can effortlessly create personalized playlists with just one click.
+
+        ✨ Personalized Song Selection:
+        Spoty GPT utilizes the power of ChatGPT to curate a tailored selection of songs based on your input. Whether it's a mood, genre, artist, or theme, our AI-driven model understands your preferences and handpicks songs to suit your unique taste.
+
+        💾 Automatic Playlist Saving:
+        Once the playlist is generated, Spoty GPT automatically saves it to your Spotify account. With just one click, you can enjoy your personalized playlist across all your devices and seamlessly integrate it into your music library.
+
+        Discover new tracks, rediscover old favorites, and let Spoty GPT enhance your musical journey. Experience the power of AI-driven song selection and effortlessly create personalized playlists that match your mood and style.
+    """
+about_content = "\n".join([line for line in ABOUT.splitlines() if line.strip()])
+
+st.set_page_config(
+    page_icon="📚",
+    page_title=TITLE,
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={"About": f"{TITLE}{about_content}"},
+)
+
+with st.sidebar:
+    st.title(TITLE)
+    st.markdown(ABOUT)
+    add_vertical_space(2)
+    st.write("💡 API keys required!!")
+
+
+if "authentication_status" not in st.session_state:
+    st.session_state.authentication_status = ""
+
+if st.session_state.authentication_status:
+    new_playlist = st.form("New Playlist")
+    with new_playlist:
+        playlist_description = st.text_area("Describe the playlist")
+        playlist_songs = st.text_input("How many songs")
+        playlist_name = st.text_input("Name your playlist")
+        playlist_button = st.form_submit_button("Submit")
+        if playlist_button:
+            with st.spinner("Creating your playlist..."):
+                new_playlist = mix.generate_playlist(
+                    playlist_description, playlist_songs
+                )
+                mix.spotify_playlist(new_playlist, playlist_name)
+                st.success("Playlist is ready!")
+                # st.cache_data.clear()
+                # st.experimental_rerun()
+
+
+# # Example usage
+# user_playlists = get_user_playlists()
+# for playlist in user_playlists:
+#     playlist_img = playlist["images"][0]["url"]
+#     playlist_name = playlist["name"]
+#     playlist_url = playlist["external_urls"]["spotify"]
